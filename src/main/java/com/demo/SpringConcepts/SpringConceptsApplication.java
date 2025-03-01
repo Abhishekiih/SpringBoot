a33107519f644cf4c186d4352b36e2d11ca7ac96
Abhishekiih <abhiibaghel1011@gmail.com>
2025-03-01 22:33:38 +0530

    UC1 Added

---
+++ src/main/java/com/demo/SpringConcepts/SpringConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.demo.SpringConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/SpringConcepts/component/HelloController.java
@@ -0,0 +1,16 @@
+package com.demo.SpringConcepts.component;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(defaultValue = "World") String name) {
+        return "Hello " + name + ", Welcome to Spring Boot!";
+    }
+}
+
